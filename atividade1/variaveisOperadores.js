aae9df214fc41d6851afc2c84e0d652812252d68
babilods <barbaraeloidossantos@gmail.com>
2025-03-07 09:00:36 -0300

    atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,12 @@
+let num_A = 100;
+let num_B = 50;
+
+let resultadoSoma = num_A + num_B;
+let resultadoSubtracao = num_A - num_B;
+let resultadoMultiplicacao = num_A * num_B;
+let resultadoDivisao = num_A / num_B;
+
+console.log(`Resultado da soma entre ${num_A} e ${num_B} é = ${resultadoSoma}`);
+console.log(`Resultado da subtração entre ${num_A} e ${num_B} é = ${resultadoSubtracao}`);
+console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} é = ${resultadoMultiplicacao}`);
+console.log(`Resultado da divisão entre ${num_A} e ${num_B} é = ${resultadoDivisao}`);localStorage
