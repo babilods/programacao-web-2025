ce26ce330f9ba88d784e560ff3c287f9b8cfd351
babilods <barbaraeloidossantos@gmail.com>
2025-03-14 09:08:17 -0300

    código corrigido

--- atividade2/verificarNumeroPrimo.js
+++ atividade2/verificarNumeroPrimo.js
@@ -1,7 +1,16 @@
 function verificarNumeroPrimo(n) {
-    if (n <= 1) return false;
-    for (let i = 2; i <= Math.sqrt(n); i++) {
-        if (n % i === 0) return false;
+    if (n <= 1){
+        return false;
+    } 
+    
+    if(n % 2 == 0 && n != 2){
+        return true;
+    }
+
+    for (let i = 3; i < n; i += 2){
+        if(n % i == 0){
+            return false;
+        }
     }
     return true;
 }
