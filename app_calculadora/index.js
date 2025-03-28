90824935c87f689f024856d0580b128690a28b10
babilods <barbaraeloidossantos@gmail.com>
2025-03-28 09:50:07 -0300

    atividade 3

---
+++ app_calculadora/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const calc = require('./calculadora');
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('Olá, mundo!')
+});
+
+
+app.get('/ola/:nome', (req, res)=>{
+    res.send(`Olá, ${req.params.nome}!`);
+
+});
+
+const PORT = 8080;
+app.listen(PORT, ()=>{
+    console.log('app rodando na porta ' + PORT);
+});
+
